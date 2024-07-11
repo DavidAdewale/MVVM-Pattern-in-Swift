50bcde9cf98264c7131d087f0438e9e2d7153b8c
DavidAdewale <adewaledavidola@gmail.com>
2024-07-11 08:24:49 +0100

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by David Adewale on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
