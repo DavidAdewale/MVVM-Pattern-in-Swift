d0f2b7f6975ff7ca9b447e4db151e0ecd4c7a210
DavidAdewale <adewaledavidola@gmail.com>
2024-07-11 11:08:50 +0100

    simple mvvm pattern example

---
+++ MVVM/Notes.swift
@@ -0,0 +1,30 @@
+//
+//  Notes.swift
+//  MVVM
+//
+//  Created by David Adewale on 11/07/2024.
+//
+
+import Foundation
+
+/**
+ 
+ Model-->
+ Model is like the brains of the application.
+ It's the business logic.
+ This is the part you'd be writing a lot of unit tests for
+ 
+ View ->
+ Represents the UI of the application.
+ Anything you can visually see with your eyes and interact with
+ 
+ View Model ->
+ Take the data from the model and provide it to the view. Basically the controller
+ 
+ View Models are not the best for SwfitUI because you;d end up with a lot of unnecessary code and add unnecessary complexity to the project
+ 
+ In Swift UI, the View is the View Model
+ 
+ 
+ 
+ */
